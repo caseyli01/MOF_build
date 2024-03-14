71af1184fcdd91d8743bef0ad1e314ff2bc68436
caseyli01 <93268539+caseyli01@users.noreply.github.com>
2024-03-14 17:37:07 +0100

    test tatradentate ligand function

--- 0MOF_build.py
+++ 0MOF_build.py
@@ -7,7 +7,7 @@ import os
 startTime = datetime.datetime.now()
 
 MLM_filename = MOF_build.build.readpdb("opt_linker.pdb")  
-frame = MOF_build.build.readpdb("FM3M_primitive3.pdb")
+frame = MOF_build.build.readpdb("FM3M_primitive.pdb")
 extra_termination = MOF_build.build.readpdb("methyl.pdb")
 extra_point_index = 1
 distance_extra_terimination = 1.7
